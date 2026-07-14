b1914332849e8f67280ddb93e6da4a3ead0b9574
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2026-07-14 09:20:21 +0200

    requested changes

--- geoserver-nonroot/build.sh
+++ geoserver-nonroot/build.sh
@@ -1,3 +1,3 @@
 docker build --build-arg GEOSERVER_UID=1000 --build-arg GEOSERVER_GID=1000 \
-  --build-arg GEOSERVER_VERSION=2.27.4-latest \
-  -t geonode/geoserver-nonroot:2.27.4-latest .
+  --build-arg GEOSERVER_VERSION=2.28.4-latest \
+  -t geonode/geoserver-nonroot:2.28.4-latest .
